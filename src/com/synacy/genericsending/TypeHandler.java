2ee5c49954d61b2497fbf5db146ac85aa3c32513
Kenichi Go Uang <kenichi.uang@synacy.com>
2017-05-05 21:17:34 +0800

    Create interface for sending any message type
    Signed-off-by: Kenichi Go Uang <kenichi.uang@synacy.com>

--- src/com/synacy/genericsending/TypeHandler.java
+++ src/com/synacy/genericsending/TypeHandler.java
@@ -1,7 +1,11 @@
 package com.synacy.genericsending;
 
+import com.synacy.FaxMessage;
+import com.synacy.SmsMessage;
+
 /**
  * Created by kenichigouang on 5/5/17.
  */
 public interface TypeHandler {
+    void send();
 }
