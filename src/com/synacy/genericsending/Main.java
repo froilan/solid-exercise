1f78b752b67b1d5b1c9fb3dce53c0b4d784c433d
JR Comision <catalino.comision@outlook.com>
2017-05-05 18:04:26 +0800

    Update classes for genericsending package

--- src/com/synacy/genericsending/Main.java
+++ src/com/synacy/genericsending/Main.java
@@ -1,22 +1,20 @@
 package com.synacy.genericsending;
 
 import com.synacy.FaxMessage;
+import com.synacy.Message;
 import com.synacy.SmsMessage;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created by froilan on 5/5/17.
- */
 public class Main {
 
 	/**
 	 * Assume this will be the messages that has to be sent and charged
 	 */
-	public static List<Object> generateMessages() {
-		return new ArrayList<Object>(Arrays.asList(new FaxMessage("631111111", "some fax message".getBytes()),
+	public static List<Message> generateMessages() {
+		return new ArrayList<>(Arrays.asList(new FaxMessage("631111111", "some fax message".getBytes()),
 				new FaxMessage("632222222", "some fax message".getBytes()),
 				new SmsMessage("633333333", "sms message"),
 				new SmsMessage("634444444", "sms message"),
@@ -29,7 +27,7 @@ public class Main {
 	 * Let's call that class GenericMessageSenderService.
 	 * */
 	public static void main (String args[]) {
-		GenericMessageSenderService genericMessageSenderService = new GenericMessageSenderService();
+		GenericMessageSenderService<Message> genericMessageSenderService = new GenericMessageSenderService();
 		generateMessages().forEach((message) -> {
 			genericMessageSenderService.send(message);
 		});
