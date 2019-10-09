e4725b51e9ecb2cf464f09248eaf888e4193ad70
Jazin Mendoza <jazin.mendoza@synacy.com>
2019-10-09 18:16:02 +0800

    Use Sendable interface to implement getDestination and generateCompressedMessage

--- src/com/synacy/genericsending/GenericMessageSenderService.java
+++ src/com/synacy/genericsending/GenericMessageSenderService.java
@@ -1,24 +1,17 @@
 package com.synacy.genericsending;
 
-import com.synacy.FaxMessage;
-import com.synacy.SmsMessage;
+import com.synacy.Sendable;
 
 /**
  * Created by froilan on 5/5/17.
  */
 public class GenericMessageSenderService {
 
-	public void send(Object message) {
-		if (message instanceof FaxMessage) {
-			String faxCompressedMessage = ((FaxMessage) message).generateCompressedMessage();
-			String destination = ((FaxMessage) message).getDestination();
-			//some fax sending specific logic here that uses destination and the compressed message.
-		} else if (message instanceof SmsMessage) {
-			String smsCompressedMessage = ((SmsMessage) message).generateCompressedMessage();
-			String destination = ((SmsMessage) message).getDestination();
-			//some fax sending specific logic here that uses destination and the compressed message.
-		}
-		//some post sending logic here...
+	public void send(Sendable message) {
+
+		String faxCompressedMessage = message.generateCompressedMessage();
+		String destination = message.getDestination();
+
 	}
 
 }
